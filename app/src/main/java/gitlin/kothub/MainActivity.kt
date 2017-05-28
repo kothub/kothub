f7fa0184b08c8fb9d422244dc8368cbebc857dc4
Knop Florian <florian_knop@hotmail.com>
2017-05-28 23:32:22 +0200

    refactor: remove MainActivity TAG

--- app/src/main/java/gitlin/kothub/MainActivity.kt
+++ app/src/main/java/gitlin/kothub/MainActivity.kt
@@ -15,11 +15,7 @@ import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.debug
 
 class MainActivity : AppCompatActivity(), AnkoLogger {
-
-    companion object {
-        val TAG = "MainActivity"
-    }
-
+    
     fun initOAuth () {
         OAuthValues.REDIRECT_URL = "oauth://kothub"
         OAuthValues.GITHUB_CLIENT = getString(R.string.github_client)
