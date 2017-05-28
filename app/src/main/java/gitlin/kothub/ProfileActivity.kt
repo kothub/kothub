18cb7742bb1f31f0a58f3a5d4cd8d09c257e3ecb
Astalaseven <gatien.bovyn@gmail.com>
2017-05-28 23:55:44 +0200

    feat: add ids in xml

--- app/src/main/java/gitlin/kothub/ProfileActivity.kt
+++ app/src/main/java/gitlin/kothub/ProfileActivity.kt
@@ -2,11 +2,48 @@ package gitlin.kothub
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import gitlin.kothub.github.OAuthValues
+import gitlin.kothub.github.api.userSummary
+import gitlin.kothub.github.requestAccessToken
+import gitlin.kothub.utilities.editSharedPreferences
+import gitlin.kothub.utilities.set
+import kotlinx.android.synthetic.main.activity_profile.*
 
 class ProfileActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+		val code = intent.data.getQueryParameter("code")
+
+		requestAccessToken(code, { error, token ->
+			if (error == null) {
+				Log.d("OAuthToken", token)
+				OAuthValues.isLoggedIn = true
+
+				OAuthValues.GITHUB_TOKEN = token!!
+
+				editSharedPreferences(getString(R.string.github_preferences_file)) {
+					set(getString(R.string.oauth_github_token) to token)
+				}
+
+				userSummary { error, summary ->
+					if (summary == null) {
+						Log.d("USERSUMMARY", "ERROR")
+						Log.d("USERSUMMARY", error?.response?.httpResponseMessage ?: "NO ERROR??")
+					}
+					else {
+						Log.d("USERSUMMARY", summary.toString())
+						followers.text = summary.followers.toString()
+//						Log.d("USERSUMMARY", summary.location)
+					}
+				}
+
+			} else {
+				// Handle error
+			}
+		})
     }
 }
