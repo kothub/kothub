307e0bf8becf486dc5fc879d5f91781a11d788ed
Astalaseven <gatien.bovyn@gmail.com>
2017-05-28 20:32:54 +0200

    feat: start Profile xml

---
+++ app/src/main/java/gitlin/kothub/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package gitlin.kothub
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
